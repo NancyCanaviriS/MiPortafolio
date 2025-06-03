76db5ded8c66ed1baf1bd99f7dfab00a9df0f43c
Nancy Canaviri <nancy.canaviri@sistemas.edu.bo>
2025-06-03 03:54:31 -0400

    avance del Juego con titulo

--- Juego.py
+++ Juego.py
@@ -32,14 +32,27 @@ font_title_32=pygame.font.Font('horror.ttf',32)
 font_title_42=pygame.font.Font('horror.ttf',42)
 font_text=pygame.font.Font('texto.ttf',24)
 
+#set title
+title_text=font_title_42.render("pildoras de la programacion",True,WHITE,BLACK)
+title_rect=title_text.get_rect()
+title_rect.centerx=WIDTH //1.5
+title_rect.y=15
+
 #configuracion del texto de puntaje
 score_text=font_text.render(f"Score:{score}",True,WHITE,BLACK)
 score_rect=score_text.get_rect()
 score_rect.topleft=(10,98)
 
+#SET LIVES
+lives_text=font_text.render("Lives:"+str(player_lives),True,WHITE,BLACK)
+lives_rect=lives_text.get_rect()
+lives_rect.topright=(WIDTH-10,90)
+
 displey = pygame.display.set_mode((HEIGHT,WIDTH))
 pygame.display.set_caption("Juego1")
+
 game_over = False
+
 # game_loop
 while not game_over:
     for event in pygame.event.get():
@@ -49,7 +62,10 @@ while not game_over:
     score_text=font_text.render(f"Score:{score}", True, WHITE,BLACK)
     lives_text=font_text.render(f"Score:{score}", True, WHITE,BLACK)
     displey.fill(BLACK)
+    displey.blit(title_text,title_rect)
     displey.blit(score_text,score_rect)
+    displey.blit(lives_text,lives_rect)
+    pygame.draw.line(displey,WHITE,(0,140),(WIDTH,140),3)
     displey.blit(mari_image,mari_rect)
     displey.blit(pelota_image,pelota_rect)
     pygame.display.update()
