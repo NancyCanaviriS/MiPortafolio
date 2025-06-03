ea8259fd8fa545ce40acc358473810e1b6039539
Nancy Canaviri <nancy.canaviri@sistemas.edu.bo>
2025-06-03 02:52:52 -0400

    avance en el juego

--- Juego.py
+++ Juego.py
@@ -5,6 +5,7 @@ pygame.init()
 HEIGHT=750
 WIDTH=480
 BLACK=(0,0,0)
+WHITE=(255,255,255)
 #imagen de la mariquita
 mari_image=pygame.image.load("mari.png")
 mari_rect=mari_image.get_rect()
@@ -16,6 +17,16 @@ pelota_rect=pelota_image.get_rect()
 pelota_rect.left=10
 pelota_rect.bottom = HEIGHT-10
 
+#fuentes del videoJuego
+font_title_32=pygame.font.Font('horror.ttf',32)
+font_title_42=pygame.font.Font('horror.ttf',42)
+font_text=pygame.font.Font('texto.ttf',24)
+
+#fuentes de videojuego
+score_text=font_text.render(f"Score:{score}",True,WHITE,BLACK)
+score_rect=score_text.get_rect()
+score_rect.topleft=(10,98)
+
 displey = pygame.display.set_mode((HEIGHT,WIDTH))
 pygame.display.set_caption("Juego1")
 game_over = False
@@ -24,9 +35,13 @@ while not game_over:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over= True
+    #actualizar los textos de puntaje y vidas
+    score_text=front_text.render(f"Score:{score}", true, WHITE,BLACK)
+    lives_text=front_text.render(f"Score:{score}", true, WHITE,BLACK)
     displey.fill(BLACK)
     displey.blit(mari_image,mari_rect)
     displey.blit(pelota_image,pelota_rect)
     pygame.display.update()
+
 
 
